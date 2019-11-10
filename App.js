d982c50d9bab7757ef585f925835d930611b550f
Mannu Tanti <maharajamannu@gmail.com>
2019-11-10 16:30:58 +0530

    first component

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+import Main from './components/MainComponent'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Main />
+    )
+  }
+}
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#0d3f34',
+//     padding: '6px',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
